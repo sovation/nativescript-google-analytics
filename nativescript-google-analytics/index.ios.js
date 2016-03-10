b84115972066f1a724efcdf6bcb2b46aa068e87f
Steve Scott <steve@sitefinitysteve.com>
2016-03-10 12:36:24 -0500

    Functional Atm, tracking to GA

--- nativescript-google-analytics/index.ios.js
+++ nativescript-google-analytics/index.ios.js
@@ -1,17 +1,13 @@
 exports.initalize = function (trackingId) {
-    //UA-74227193-1
     var configureError = new interop.Reference();
     GGLContext.sharedInstance().configureWithError(configureError);
     
     var gai = GAI.sharedInstance();
     gai.trackUncaughtExceptions = true;
-    //gai.trackerWithTrackingId(trackingId);
     global.tracker = GAI.sharedInstance().defaultTracker;
-    /*
-    debugger;
+    
     // Only use this in debug mode
-    gai.logger.logLevel = kGAILogLevelVerbose;
-    */
+    //gai.logger.logLevel = kGAILogLevelVerbose;
 }
 
 exports.logView = function(viewname){
@@ -25,40 +21,20 @@ exports.logView = function(viewname){
 }
 
 exports.logEvent = function(data){
-
-    
     var event = GAIDictionaryBuilder.createEventWithCategoryActionLabelValue(
       data.category,
       data.action,
       data.label,
       data.value  
     );
+    var builtEvent = event.build();
+    debugger;
+    //Future implimentation
+    //GAITracker.prototype.send.call(global.tracker, builderResult);
     
-    var eventData = event.build();
-    global.tracker.send = eventData;
-        debugger;
-    console.log("Event Sent");
-    
-    /*
-    var event = new com.google.android.gms.analytics.HitBuilders.EventBuilder();
+    //Current Hack
+    global.tracker.performSelectorWithObject("send:", builtEvent)
 
-    if(data.category && data.category !== "" && data.category !== null)
-        event.setCategory(data.category);
-        
-    if(data.action && data.action !== "" && data.action !== null)
-        event.setAction(data.action);
-    
-    if(data.label && data.label !== "" && data.label !== null)
-        event.setLabel(data.label);
-     
-    if(data.value && data.value !== "" && data.value !== null)
-        event.setValue(data.value);   
-    
-    var bulitEvent = event.build();
-    
-    global.tracker.send(bulitEvent);
-    */
-    
 }
 
 

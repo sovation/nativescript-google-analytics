274f092d8d078d3c77a440e811108e2fc4999b46
Guillaume ROY <gr.guillaume.roy@gmail.com>
2016-04-28 17:37:49 +0200

    Fixed TS definition file

--- plugin/index.d.ts
+++ plugin/index.d.ts
@@ -1,43 +1,41 @@
-declare module "nativescript-google-analytics" {
-    export interface InitalizeOptions {
-        dispatchInterval?:number;
-        logging?: boolean;
-        trackingId: string;
-    }
-
-    export interface LogEventOptions {
-        action: string;
-        category: string;
-        label?: string;
-        value?: number;
-    }
-
-    export interface LogExceptionOptions {
-        description: string;
-        fatal?: boolean;
-    }
-
-    export interface StartTimerOptions {
-        category: string,
-        label?: string;
-        name?: string;
-    }
-
-    export interface LogTimingEventOptions {
-        category: string,
-        label?: string;
-        name?: string;
-        value: number;
-    }
-
-    export function initalize(options: InitalizeOptions): void;
-    export function logEvent(options: LogEventOptions): void;
-    export function logView(viewName: string): void;
-    export function dispatch(): void;
-    export function logException(options: LogExceptionOptions): void;
-    export function logException(description: string): void;
-    export function startTimer(timerName: string, options: StartTimerOptions): void;
-    export function stopTimer(timerName: string): void;
-    export function logTimingEvent(options: LogTimingEventOptions): void;
-    export function getTracker(): string;
+export interface InitalizeOptions {
+    dispatchInterval?:number;
+    logging?: boolean;
+    trackingId: string;
 }
+
+export interface LogEventOptions {
+    action: string;
+    category: string;
+    label?: string;
+    value?: number;
+}
+
+export interface LogExceptionOptions {
+    description: string;
+    fatal?: boolean;
+}
+
+export interface StartTimerOptions {
+    category: string,
+    label?: string;
+    name?: string;
+}
+
+export interface LogTimingEventOptions {
+    category: string,
+    label?: string;
+    name?: string;
+    value: number;
+}
+
+export function initalize(options: InitalizeOptions): void;
+export function logEvent(options: LogEventOptions): void;
+export function logView(viewName: string): void;
+export function dispatch(): void;
+export function logException(options: LogExceptionOptions): void;
+export function logException(description: string): void;
+export function startTimer(timerName: string, options: StartTimerOptions): void;
+export function stopTimer(timerName: string): void;
+export function logTimingEvent(options: LogTimingEventOptions): void;
+export function getTracker(): string;
